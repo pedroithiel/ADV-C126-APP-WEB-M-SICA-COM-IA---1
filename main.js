4726299fbfc99599a24a380ba5b57e44af533c76
pedroithiel <101153331+pedroithiel@users.noreply.github.com>
2023-01-03 15:43:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+som1 = "";
+som2 = "";
+
+function preload() {
+    som1 = loadSound("music.mp3");
+    som2 = loadSound("music2.mp3");
+
+}
+
 function setup() {
     canvas = createCanvas(400,400);
     canvas.center();
